deb0f05e1a27116042cd5c6bf041eec675b6befc
cheshire-howe <joshzoff@gmail.com>
2014-11-03 11:50:34 -0500

    Moved Api controllers to separate folder

--- AutofacGenericRepositoryMvc.Web/Api/CountryApiController.cs
+++ AutofacGenericRepositoryMvc.Web/Api/CountryApiController.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using AutofacGenericRepositoryMvc.Model.Concrete;
 using AutofacGenericRepositoryMvc.Service.Interfaces;
 
-namespace AutofacGenericRepositoryMvc.Web.Controllers
+namespace AutofacGenericRepositoryMvc.Web.Api
 {
     public class CountryApiController : ApiController
     {
